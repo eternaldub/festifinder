68446f748320a5085664dc9b65003be81725dec9
eternaldub <tim.garrett17@gmail.com>
2017-06-17 00:03:37 -0500

    add unittesting

---
+++ FestFinder/tests/fest_tests.py
@@ -0,0 +1,35 @@
+import os
+import festfinder
+import unittest
+import tempfile
+
+class festfinderTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, festfinder.app.conffig['DATABASE'] = tempfile.mkstemp()
+        festfinder.app.testing = True
+        self.app = festfinder.app.test_client()
+        with festfinder.app.test_client()
+        with festfinder.app.app_context():
+            festfinder.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(festfinder.app.config['DATABASE'])
+
+if __name__ == '__main__':
+    unittest.main()
+class FestfinderTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, festfinder.app.config['DATABASE'] = tempfile.mkstemp()
+        festfinder.app.testing = True
+        self.app = festfinder.app.test_client()
+        with festfinder.app.test_client():
+            festfinder.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(festfinder.app.config['DATABASE'])
+
+        
